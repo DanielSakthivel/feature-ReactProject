a90dee0dbc7bca59d362fa34ab4a5d324bfb15de
DanielSakthivel <danielsakthivel1996@gmail.com>
2023-08-29 11:33:03 +0530

    Day4 TAsk Completed

---
+++ Day4TaskResume.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "Sakthivel",
+      "email": "Sakthivel.M",
+      "phone": 8838114812,
+      "degree": "B.E",
+      "location": {
+        "address": "15/16 Sainath Enclave ShenoyNagar",
+        "postalCode": 600126,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/daniel/",
+          "github":"https://github.com/Sakthivel"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "The Photophilehub PVT",
+        "position": "Business Executive",
+        "startDate": "2020-11-04",
+        "endDate": "2021-08-15",
+        "summary": "i am the business executive handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Anna university ",
+        "department": "Civil",
+        "studyType": "fulltime",
+        "batch start year": 2014,
+        "batch end year": 2018,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+        "project": [
+          "automatic attendance maintaing system using python language"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,youtuber,",
+      }
+    ]
+  }
+  console.log(myResume);
